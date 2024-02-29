c31991782a5e222b0da013954dc24ffd216ba516
RajashekharReddyBheemreddy <bheemreddyrajashekhar@gmail.com>
2024-02-28 19:29:29 -0500

    make it better

--- backend/server.js
+++ backend/server.js
@@ -1,4 +1,4 @@
-const path = require('path')
+const path = require('path');
 const express = require('express')
 const colors = require('colors')
 const { errorHandler } = require('./middleware/errorMiddleware')
@@ -17,15 +17,18 @@ app.use(express.urlencoded({extended: false}))
 app.use('/api/goals', require('./routes/goalRoutes'))
 app.use('/api/users', require('./routes/userRoutes'))
 
-// Serve Frontend
-if(process.env.NODE_ENV === 'production'){
-    app.use(express.static(path.join(__dirname, '../frontend/build')))
-
-    app.get('*', (req, res) => res.sendFile(path.resolve(__dirname, '../', 'frontend', 'build', 'index.html')))
-}
-else{
-    app.get('/', (req, res) => res.send('Please set to Production in .env'))
-}
+// Serve frontend
+if (process.env.NODE_ENV === 'production') {
+    app.use(express.static(path.join(__dirname, '../frontend/build')));
+  
+    app.get('*', (req, res) =>
+      res.sendFile(
+        path.resolve(__dirname, '../', 'frontend', 'build', 'index.html')
+      )
+    );
+  } else {
+    app.get('/', (req, res) => res.send('Please set to production'));
+  }
 
 // error handler
 app.use(errorHandler)
